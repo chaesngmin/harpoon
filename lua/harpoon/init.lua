804bd422c844330f5da0441919b04279ebda8294
chaes <chaesngmin@gmail.com>
2024-01-04 01:32:08 -0800

    !!! fix(cmd): correct syntax fore autocmd events

--- lua/harpoon/init.lua
+++ lua/harpoon/init.lua
@@ -10,12 +10,10 @@ local cache_config = string.format("%s/harpoon.json", data_path)
 
 local M = {}
 
-local the_primeagen_harpoon = vim.api.nvim_create_augroup(
-    "THE_PRIMEAGEN_HARPOON",
-    { clear = true }
-)
+local the_primeagen_harpoon =
+    vim.api.nvim_create_augroup("THE_PRIMEAGEN_HARPOON", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufLeave, VimLeave" }, {
+vim.api.nvim_create_autocmd({ "BufLeave", "VimLeave" }, {
     callback = function()
         require("harpoon.mark").store_offset()
     end,
